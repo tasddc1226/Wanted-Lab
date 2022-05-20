b1e2387e8139408fef24e6cc39573de2b4dd6294
tasddc1226 <tasddc@naver.com>
2022-05-20 18:37:50 +0900

    ADD: django-filter app 추가

--- wanted_lab/settings.py
+++ wanted_lab/settings.py
@@ -45,10 +45,12 @@ INSTALLED_APPS = [
     'company.apps.CompanyConfig',
     # rest app
     'rest_framework',
+    'django_filters',
 ]
 
 # DRF Pagination 전역 설정
 REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5
 }
