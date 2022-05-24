c9344b11445dae610ffb81d23371d6575f230859
tasddc1226 <tasddc@naver.com>
2022-05-25 00:22:15 +0900

    FEAT: Serializer 추가 및 회사명 자동완성 회사명 검색 기능구현

--- company/serializers.py
+++ company/serializers.py
@@ -2,8 +2,13 @@ from rest_framework import serializers
 from .models import *
 
 class CompanySerializer(serializers.ModelSerializer):
+    company_name = serializers.CharField(source="name")
     class Meta:
         model = CompanyName
-        fields = [
-            'name'
-        ]
+        fields = ['company_name']
+
+class CompanyRetrieveSerializer(serializers.ModelSerializer):
+    company_name = serializers.CharField(source="name")
+    class Meta:
+        model = CompanyName
+        fields = ['company_name', 'tags']
